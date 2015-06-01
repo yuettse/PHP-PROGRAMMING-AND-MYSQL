f731b5fd96b29a37c3fdfd4d888478b427f519cb
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-01 16:11:29 -0700

    Create assignment17.php

---
+++ assignment17.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Box</title>
+<meta charset="UTF-8">
+</head>
+
+<body>
+<?php
+	$blocknum = 8;
+	$blocksize = 10;
+	function($size, $num) {
+		for ( $i = 0; $i < $num; i++ ) {
+			echo '<div class = "rows">';
+			for ( $a = 0; $a < $num; a99) {
+				echo '<div class = "blocks" style="width="' . $size . 'px">'
+			}
+		}
+	}
+?>
+</body>
+</html>
