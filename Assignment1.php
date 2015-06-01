5ecf5511f2768cd760ce15e533cb81e59cdf0452
Yuet Fu Tse <yuettse@hotmail.com>
2015-05-31 20:11:31 -0700

    Create Assignment1.php

---
+++ Assignment1.php
@@ -0,0 +1,9 @@
+<?php
+	//Create a program that prints all the odd numbers from 1 to 1000. Use the for loop and don't use any array to do this exercise.
+
+	for($i = 0; $i <= 1000; $i++) {
+		if ( $i % 2 == 1) {
+			echo $i . '<br>';
+		}
+	}
+?>
