88d8386cd561ddca515709c19c511bb0c751f2ec
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-01 18:16:23 -0700

    Update assignment 13 throwing coins.php

--- assignment 13 throwing coins.php
+++ assignment 13 throwing coins.php
@@ -1,26 +1,20 @@
 <?php
-
 //You're going to create a program that simulates throwing a coin 5,000 times. Your program should display how many times the head/tail appears.
-
 function throwing($howmanytimes) {
 	$coin = 0;
 	$head = 0;
 	$tail = 0;
-	for($i = 0; $i < $howmanytimes; i++) {
+	for($i = 0; $i < $howmanytimes; $i++) {
 		$coin = rand(1,2);
-		if( coin == 1) {
-			head++;
-			echo "Attempt #1: Throwing a coin... It's head!... Got " 
-			. $head . "head(s) so far and " . $tail . "(s) so far";
-		}
+		if( $coin == 1) {
+			$head++;
+			echo "Attempt #1: Throwing a coin... It's head!... Got " . $head . "head(s) so far and " . $tail . "(s) so far<br>";
 		} else {
-			tail++;
-			echo "Attempt #1: Throwing a coin... It's tail!... Got " 
-			. $head . "head(s) so far and " . $tail . "(s) so far";		
+			$tail++;
+			echo "Attempt #1: Throwing a coin... It's tail!... Got " . $head . "head(s) so far and " . $tail . "(s) so far<br>";		
 		}
 	}
 }
 
 throwing(5000);
-
 ?>
