121072b9d6aeafdac579cc4e1a4a31ed61dedfcf
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-02 16:39:46 -0700

    Create assignment 1 advance .php

---
+++ Advance-PHP-Assignment-1/assignment 1 advance .php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+	<title>Yuet PHP GET AND POST TEST</title>
+	<meta charset="UTF-8";>
+</head>
+
+<body>
+
+	<form action="getpost.php" method="get">
+		User Name: <input type="text" name="user" placeholder="Enter your User Name here">
+		Comment: <input type="textarea" name="comment" placeholder="Enter your comment">
+		<input type="submit" value="submit">
+		
+	</form>
+
+</body>
+</html>
