3aeee9acb2ad982ebf171b449c2807b0e1d753aa
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-04 14:06:41 -0700

    Update number.php

--- greater-number-game/number.php
+++ greater-number-game/number.php
@@ -2,16 +2,22 @@
 session_start();
 	if($_GET['again'] == 'yes') {
 		$_SESSION = array();
+		$_GET['again'] = 'no';
 		header('Location: greatnumbergame.php');
 	}
 	
-	if ( isset($_GET['number']) {
+	if ( $_GET['number'] == TRUE ) {
 		if($_GET['number'] > $_SESSION['guessnumber']) {
 			$_SESSION['answer'] = 'high';
+			$_SESSION['times']++;
+			unset($_SESSION['number']);
 		} else if ($_GET['number'] < $_SESSION['guessnumber']) {
 			$_SESSION['answer'] = 'low';
+			$_SESSION['times']++;
+			unset($_SESSION['number']);
 		} else {
 			$_SESSION['answer'] = 'right';
+			$_SESSION['times']++;
 		}
 	}
 	header('Location: greatnumbergame.php');
