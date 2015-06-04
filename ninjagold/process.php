f9370abbca922587ff2df00c71d4c20873880d83
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-03 22:41:28 -0700

    Create process.php

---
+++ ninjagold/process.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if( !isset($_SESSION['gold']) {
+	$_SESSION['gold'] = 0;
+}
+$_SESSION['temp'] = 0;
+if( $_GET['building'] == 'farm' ) {
+	$_SESSION['temp'] = rand(10,20);
+	$_SESSION['gold'] += $_SESSOPN['temp'];
+} else if ( $_GET['building'] == 'cave' ) {
+	$_SESSION['temp'] = rand(5,10);
+	$_SESSION['gold'] += $_SESSOPN['temp'];
+} else if ( $_GET['building'] == 'house' ) {
+	$_SESSION['temp'] = rand(2,5);
+	$_SESSION['gold'] += $_SESSOPN['temp'];
+} else {
+	$_SESSION['temp'] = rand(0,1);
+	if ( $_SESSION['temp'] == 0 ) {
+		$_SESSION['temp'] = rand(0,50);
+		$_SESSION['gold'] += $_SESSOPN['temp'];
+	} else {
+		$_SESSION['temp'] = rand(0,50) * -1;
+		$_SESSION['gold'] += $_SESSOPN['temp'];
+	}
+}
+
+if ( $_SESSION['gold'] < 0) {
+	array_push($_SESSION['blog'], "<div>You entered a casino and lost " . $_SESSION['temp'] * -1 . "gold" . date("Y-m-d") . ' ' . date("h:i:sa"). '</div>';
+} else {
+	array_push($_SESSION['blog'], "<div>You entered a" . $_SESSION['building'] . "and earned " . $_SESSION['temp'] . "gold" . date("Y-m-d") . ' ' . date("h:i:sa"). '</div>';
+}
+
+
+
+
+
+
+
+		<?
+		if ($_SESSION['temp'] < 0) {
+			
+		} else {
+			
+		}
+		?>
+header('LocationLocation: ninjagoldgame.php');
+
+
+
+
+
+?>
