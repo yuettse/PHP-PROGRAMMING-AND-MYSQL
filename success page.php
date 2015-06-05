ade009f92d5bf67dbc43b67264e875d2103dbb0b
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-05 16:41:16 -0700

    Create success page.php

---
+++ success page.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Yuet Success Page</title>
+	<meta chatset="UTF-8">
+</head>
+<body>
+	<div id="container" style="height:100px; width:500px; background-color: green;"></div>
+	
+
+</body>
+</html>
